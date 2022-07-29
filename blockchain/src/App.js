becb3d34eb3ebf3bfa4761b46816c4d5d4d1dc60
Nachiketlele <nachiketlele75@gmail.com>
2022-07-29 16:56:58 +0530

    ASD

--- blockchain/src/App.js
+++ blockchain/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from "./components/Navbar.jsx"
+import MainRoutes from './mainroutes/MainRoutes';
 function App() {
   return (
     <div className="App">
-     
+        <Navbar/>
+        <MainRoutes/>
     </div>
   );
 }
